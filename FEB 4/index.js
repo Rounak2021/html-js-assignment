862b569853c611fe9ed45be297ae22686baec204
Rounak Chakraborty <rounak17chakraborty@gmail.com>
2022-02-06 15:59:34 +0530

    Unsplash Photo Gallery

---
+++ FEB 4/index.js
@@ -0,0 +1,27 @@
+let photo_album=document.getElementById("photo-album")
+const my_KEY = "Empw2mA9hLTL3Ks6DdPjG-HA6n4NTKdqrnsGUBo2tEs";
+
+const my_SECRET = "CZqD7mrJPjpUphCldFgk0pe19PqCH1RE2qeY9cHJQWU";
+let input = document.getElementsByTagName("input")[0];
+input.addEventListener("input", ()=>{
+photo_album.innerHTML = "";
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${my_KEY}&query=${input.value}&per_page=20`)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(res) {
+       
+        for(let i = 0; i < res.results.length; i = i + 1) {
+            let single_result = res.results[i];
+            let single_url = single_result.urls.regular;
+            console.log(single_url);
+            img_ref=document.createElement("img")
+            img_ref.src=single_url
+            photo_album.appendChild(img_ref)   
+        }
+    })
+    
+});
+console.log(input);
+
+
